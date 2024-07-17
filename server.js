bdf8575d1c99b03e1cf774438f72c50e4e6538d0
Renato Cordova <ren99cordova@gmail.com>
2024-07-17 14:19:10 -0400

    serving public files using path

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
