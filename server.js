0cdadf71ac3527244d9bd672f53b12495300aebe
Renato Cordova <ren99cordova@gmail.com>
2024-08-15 13:45:00 -0400

    waking vercel up

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const legoData = require("./modules/legoSets");
 const express = require("express");
 const app = express();
 const path = require("path");
-
+//
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
